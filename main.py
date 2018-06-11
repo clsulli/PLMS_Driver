0dcae667212427ad825666572a272278b30d8c0a
clsulli <clsulli@siue.edu>
2018-06-10 21:24:28 -0500

    dont konw

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def printAccountQRCode(data):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
+        box_size=4,
+        border=2,
     )
     qr.add_data(data)
     qr.make(fit=True)
